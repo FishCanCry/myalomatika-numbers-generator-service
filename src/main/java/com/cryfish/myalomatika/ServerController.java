2e782cd0fc1d9358dd65da0b20fe7448fcd8a630
Pavel Pecherskikh <pvlpech@gmail.com>
2020-09-26 00:13:05 +0400

    CrossOrigin annotation was removed

--- src/main/java/com/cryfish/myalomatika/ServerController.java
+++ src/main/java/com/cryfish/myalomatika/ServerController.java
@@ -1,13 +1,11 @@
 package com.cryfish.myalomatika;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.apache.log4j.Logger;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ServerController {
 
