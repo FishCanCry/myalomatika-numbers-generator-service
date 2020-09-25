3bb8cce680cef72c7ad32262e456751fa4b02e44
Pavel Pecherskikh <pvlpech@gmail.com>
2020-09-26 00:27:24 +0400

    CrossOrigin annotation was restored with * value

--- src/main/java/com/cryfish/myalomatika/ServerController.java
+++ src/main/java/com/cryfish/myalomatika/ServerController.java
@@ -1,11 +1,13 @@
 package com.cryfish.myalomatika;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.apache.log4j.Logger;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ServerController {
 
